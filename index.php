1fc9c3fff66b2b3b38f67f9d4260b4a7dd224437
alnever <Teivas@1871>
2021-08-26 17:23:21 +0500

    + posts layout

--- index.php
+++ index.php
@@ -21,8 +21,47 @@
 
         <div class="posts">
             <!-- sticky posts -->
+            <?php
+                $sticky = get_option('sticky_posts');
+                $args = [
+                    'post__in' => $sticky,
+                    'ignore_sticky_posts' => 5,
+                ];
+                $query = new WP_Query($args);
+                if ( $query->have_posts() ): ;
+            ?>
             <div class="posts-sticky">
+                    <?php
+
+                        while ( $query->have_posts() ): $query->the_post();
+                    ?>
+                        <article class="post-sticky" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                                <div class="post-sticky-title"> <?php the_title(); ?> </div>
+
+                                <div class="post-sticky-date">
+                                    <?php echo get_the_date('d.m.Y'); ?>
+                                </div>
+
+                                <div class="post-sticky-content">
+                                    <?php the_excerpt(); ?>
+                                </div>
+
+                                <?php if (has_category()): ?>
+                                    <div class="post-sticky-categories">
+                                        <img src="<?php echo get_theme_file_uri('/icons/category.svg'); ?>" class="swtk-icon">
+                                        <?php the_category(); ?>
+                                    </div>
+                                <?php endif; ?>
+
+                        </article>
+                    <?php
+                        endwhile;
+                        wp_reset_postdata();
+                    ?>
+
             </div>
+            <?php endif; ?>
 
             <!-- main cycle -->
             <?php
